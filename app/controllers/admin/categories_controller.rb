c5b0bc0fc8d9853f64e55fd5fd13b47ea9199eb5
rdgoite <rdgoite@gmail.com>
2013-08-27 01:17:31 +0800

    Initial working code for create category.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,7 +4,7 @@ class Admin::CategoriesController < Admin::BaseController
   def index; redirect_to :action => 'new' ; end
   def edit; new_or_edit;  end
 
-  def new 
+  def new
     respond_to do |format|
       format.html { new_or_edit }
       format.js { 
@@ -25,8 +25,8 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = Category.find(params[:id])
-    @category.attributes = params[:category]
+    @category = Category.get_or_build params[:id]
+    @category.attributes = params[:category] if @category
     if request.post?
       respond_to do |format|
         format.html { save_category }
--- app/models/category.rb
+++ app/models/category.rb
@@ -81,6 +81,11 @@ class Category < ActiveRecord::Base
     self.permalink = self.name.to_permalink if self.permalink.nil? or self.permalink.empty?
   end
 
+  def self.get_or_build id = nil
+    category = Category.find(id) if id
+    category = Category.new unless category
+  end
+  
   validates_presence_of :name
   validates_uniqueness_of :name, :on => :create
 end
